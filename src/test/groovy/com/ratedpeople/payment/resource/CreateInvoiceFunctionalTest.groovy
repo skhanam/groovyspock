324778a821698ad3cd4e194c682bf23326799b3a
Shabana Khanam <shabana.khanam@ratedpeople.com>
2015-03-11 10:52:15 +0000

    Update create invoice functional test EXPR-35

--- src/test/groovy/com/ratedpeople/payment/resource/CreateInvoiceFunctionalTest.groovy
+++ src/test/groovy/com/ratedpeople/payment/resource/CreateInvoiceFunctionalTest.groovy
@@ -80,63 +80,6 @@ class CreateInvoiceFunctionalTest extends AbstractUserTokenTest{
 	
 	
 	
-	def "test payment from user "(){
-		
-		given:
-		String responseStatus = null
-		def json = new JsonBuilder()
-		json {
-			"fromUserId" DataValues.requestValues.get("FROMUSERID")
-			"toUserId" DataValues.requestValues.get("TOUSERID")
-			"jobId" DataValues.requestValues.get("JOBID")
-			"token" "528d5b2a1ed64d87854d2920dd7f5560"
-			"currency" DataValues.requestValues.get("CURRENCY")
-			"skrillTransaction" DataValues.requestValues.get("SKRILLTRANSACTION")
-			"amount" DataValues.requestValues.get("AMOUNT")
-			"fromUserEmail" DataValues.requestValues.get("FROMUSEREMAIL")
-			"ip" DataValues.requestValues.get("IP")
-		
-		}
-		
-		println "Json is " +  json.toString()
-	when:
-		HTTP_BUILDER.request(Method.PUT, ContentType.JSON){
-			uri.path = DataValues.requestValues.get("PAYMENTSERVICE")+"v1.0/users/1/jobs/1"
-			headers.'Authorization' = "Bearer "+ ACCESS_TOKEN
-			body = json.toString()
-			requestContentType = ContentType.JSON
-			
-			println "Uri : " + uri
-			response.success = { resp, reader ->
-				println "Success"
-				println "Got response: ${resp.statusLine}"
-				println "Content-Type: ${resp.headers.'Content-Type'}"
-				
-				responseStatus = resp.statusLine.statusCode
-				
-				reader.each{
-					println "Token values : "+"$it"
-					
-					String token = "$it"
-					String key = token.substring(0, token.indexOf("="))
-					String value = token.substring(token.indexOf("=") + 1, token.length())
-					println key
-					println value
-				}
-			}
-			
-			response.failure = { resp ->
-				println "Request failed with status ${resp.status}"
-				responseStatus = resp.statusLine.statusCode
-			}
-		}
-	then:
-		responseStatus == DataValues.requestValues.get("STATUS200")
-//		where:
-//			status
-//			DataValues.requestValues.get("STATUS200")
-//			DataValues.requestValues.get("STATUS400")	|DataValues.requestValues.get("MERCHANTEXISTS")
-	}
-
+	
 	
 }
