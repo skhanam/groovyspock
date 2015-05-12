c57766940c4787431a66bcd9e63a9fcf8075c652
Shabana Khanam <shabana.khanam@ratedpeople.com>
2015-05-12 14:54:08 +0100

    raise an invoice

--- src/test/groovy/com/ratedpeople/job/resource/TMJobList.groovy
+++ src/test/groovy/com/ratedpeople/job/resource/TMJobList.groovy
@@ -185,10 +185,16 @@ class TMJobList extends AbstractUserToken {
 	def "Get Single Job List Tradesman"(){
 		given:
 		String responseStatus = null
+		def jobId = DatabaseHelper.select("select id  from job.job where job_status_id = 1 limit 1")
+		if (jobId.startsWith("[{id")){
+			jobId = jobId.replace("[{id=", "").replace("}]","")
+			println ("Job Id is : " +jobId)
+		}
+		
 		when:
 		HTTP_BUILDER.request(Method.GET, ContentType.JSON){
 			headers.'Authorization' = "Bearer "+ ACCESS_TOKEN_TM
-			uri.path =  JOB_URI_PREFIX +USER_ID_TM+"/tmjobs/1"
+			uri.path =  JOB_URI_PREFIX +USER_ID_TM+"/tmjobs/"+jobId
 			println "uri job is : "+uri.path
 			requestContentType = ContentType.JSON
 			
@@ -220,5 +226,75 @@ class TMJobList extends AbstractUserToken {
 		then:
 		responseStatus == CommonVariable.STATUS_200
 		}
+	
+	
+	
+	/*
+	 * Need to change this test when fully implemented for start & complete status
+	 * change the query
+	 */
+	
+	def "Raise an Invoice by Tradesman"(){
+		given:
+		String responseStatus = null
+		def jobRaiseInvoice = DatabaseHelper.select("select id  from job.job where job_status_id = 2 limit 1")
+		if (jobRaiseInvoice.startsWith("[{id")){
+			jobRaiseInvoice = jobRaiseInvoice.replace("[{id=", "").replace("}]","")
+			println ("Job Id is : " +jobRaiseInvoice)
+		}
+		def json = new JsonBuilder()
+		json {
+			"jobId" jobRaiseInvoice
+			"hoursWorked" '1.5'
+		}
+		when:
+		HTTP_BUILDER.request(Method.PUT, ContentType.JSON){
+			headers.'Authorization' = "Bearer "+ ACCESS_TOKEN_TM
+			uri.path =  JOB_URI_PREFIX +USER_ID_TM+"/tmjobs/"+jobRaiseInvoice+"/invoice"
+			println "uri job is : "+uri.path
+			requestContentType = ContentType.JSON
+			body = json.toString()
+			println "Uri : " + uri
+			response.success = { resp, reader ->
+				println "Success"
+				println "Got response: ${resp.statusLine}"
+				println "Content-Type: ${resp.headers.'Content-Type'}"
+				
+				responseStatus = resp.statusLine.statusCode
+				
+				reader.each{
+					println "Token values : "+"$it"
+					
+					String token = "$it"
+					String key = token.substring(0, token.indexOf("="))
+					String value = token.substring(token.indexOf("=") + 1, token.length())
+					println key
+					println value
+				}
+			}
+			
+			response.failure = { resp, reader ->
+				println "Request failed with status ${resp.status}"
+				println " stacktrace : "+reader.each{"$it"}
+				responseStatus = resp.statusLine.statusCode
+			}
+		}
+		then:
+		responseStatus == CommonVariable.STATUS_200
+		}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	
 }
