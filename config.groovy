4562814cbeff0c9bc3b0c6f0f3485d4e41e94dc6
Shabana Khanam <shabana.khanam@ratedpeople.com>
2015-05-27 16:57:21 +0100

    GID - 108 updated the post a job test adding longtitude & latitude

---
+++ config.groovy
@@ -0,0 +1,20 @@
+/**
+ * @author shabana.khanam
+ *
+ */
+class config {
+	
+	environments {
+		
+		minerva {
+			serverName = 'http://minerva.ratedcloud.net:8765'
+		}
+		
+		dev {
+				serverName = 'http://davide-devbox.ratedcloud.net:8765'
+			}
+		
+		}
+	
+
+}
---
+++ src/test/groovy/com/ratedpeople/support/config.groovy
@@ -0,0 +1,20 @@
+package com.ratedpeople.support
+/**
+ * 
+ */
+
+/**
+ * @author shabana.khanam
+ *
+ */
+environments {
+		minerva {
+			serverName = 'http://minerva.ratedcloud.net:8765'
+		}
+		
+		davide {
+			serverName = 'http://davide-devbox.ratedcloud.net:8765'
+			
+		}
+		
+}
