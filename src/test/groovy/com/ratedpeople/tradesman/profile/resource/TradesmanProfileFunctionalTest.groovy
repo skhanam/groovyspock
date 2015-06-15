d34d037d352578bafdf20d49b69e61295eaec966
Shabana Khanam <shabana.khanam@ratedpeople.com>
2015-06-15 14:54:34 +0100

    GID-344 functional test for regenerate email token and verify email

--- src/test/groovy/com/ratedpeople/tradesman/profile/resource/TradesmanProfileFunctionalTest.groovy
+++ src/test/groovy/com/ratedpeople/tradesman/profile/resource/TradesmanProfileFunctionalTest.groovy
@@ -673,7 +673,7 @@ class TradesmanProfileFunctionalTest extends AbstractTradesman {
 	
 	
 	def "Update a Tradesman Availability"(){
-		given:
+	given:
 			String responseCode = null
 			def json = new JsonBuilder()
 			json {
@@ -740,7 +740,73 @@ class TradesmanProfileFunctionalTest extends AbstractTradesman {
 	
 	
 	
+	def "Regenerate Email validation Token"(){
+			given:
+					String responseCode = null
+					
+		    when:
+				println "********************************"
+				println "Test Running .... Regenerate Email validation Token"
+					HTTP_BUILDER.request(Method.PUT,ContentType.JSON){
+						uri.path = PROFILE_PREFIX +USER_ID_DYNAMIC_TM  + "/email/resettoken"
+						headers.'Authorization' = "Bearer "+ ACCESS_TOKEN_DYNAMIC_TM
+						requestContentType = ContentType.JSON
+						println "Uri is " + uri
+						response.success = { resp, reader ->
+							println "Success"
+							println "Got response: ${resp.statusLine}"
+							println "Content-Type: ${resp.headers.'Content-Type'}"
+							responseCode = resp.statusLine.statusCode
+							reader.each{ "Results  : "+ "$it" }
+						}
+						response.failure = { resp, reader ->
+							println " stacktrace : "+reader.each{"$it"}
+							println 'Not found'
+							responseCode = resp.statusLine.statusCode
+						}
+					}
+				then:
+					responseCode == CommonVariable.STATUS_200
+	}
 	
+	
+	def "Validate Email with Token"(){
+		given:
+				String responseCode = null
+				def getToken = DatabaseHelper.select("select token from tmprofile.email_validation_token where user_id = '${USER_ID_DYNAMIC_TM}'")
+				println "Token for email  is :"+getToken
+				if (getToken.startsWith("[{token")){
+					getToken = getToken.replace("[{token=", "").replace("}]","")
+					println "Token is  is : " +getToken
+				}
+				
+		when:
+			println "********************************"
+			println "Test Running ....  Validate Email with Token"
+				HTTP_BUILDER.request(Method.PUT,ContentType.JSON){
+					uri.path = PROFILE_PREFIX +USER_ID_DYNAMIC_TM  + "/email/verify"
+					uri.query = [
+						token:getToken
+						]
+					headers.'Authorization' = "Bearer "+ ACCESS_TOKEN_DYNAMIC_TM
+					requestContentType = ContentType.JSON
+					println "Uri is " + uri
+					response.success = { resp, reader ->
+						println "Success"
+						println "Got response: ${resp.statusLine}"
+						println "Content-Type: ${resp.headers.'Content-Type'}"
+						responseCode = resp.statusLine.statusCode
+						reader.each{ "Results  : "+ "$it" }
+					}
+					response.failure = { resp, reader ->
+						println " stacktrace : "+reader.each{"$it"}
+						println 'Not found'
+						responseCode = resp.statusLine.statusCode
+					}
+				}
+			then:
+				responseCode == CommonVariable.STATUS_200
+ }
 	
 	
 	private def getAddress(String additionalInfo){
