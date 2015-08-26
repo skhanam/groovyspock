8d733263a85f54e93cd8c0243c59fb6f24722a25
Davide Porracchio <davide.porracchio@ratedpeople.com>
2015-08-26 14:38:31 +0100

    updated code

--- src/test/groovy/com/ratedpeople/integration/JobFlow/JobFlowIntegrationTest.groovy
+++ src/test/groovy/com/ratedpeople/integration/JobFlow/JobFlowIntegrationTest.groovy
@@ -19,19 +19,25 @@ import groovyx.net.http.Method
 class JobFlowIntegrationTest extends AbstractHomeowner{
 
 	private static String getpinToken
+	private static String phoneid
 	private static final String JOB_URI_PREFIX = CommonVariable.JOB_SERVICE_PREFIX + "v1.0/users/"
 	
 	def "JobFlow Integration"(){
 		given :
+			Thread.sleep(5000)
+			postPhone(USER_ID_DYNAMIC_HO)
+			Thread.sleep(5000)
 			validatePIN();
 			String responseStatus = null
 			def json = new JsonBuilder()
-			json{ "pinToken" getpinToken }
+			json{ 
+				"phoneId" phoneid
+				"pinToken" getpinToken }
 			println "Json is    : "+json.toString()
 			
 		when:
 			HTTP_BUILDER.request(Method.PUT,ContentType.JSON){
-				uri.path = CommonVariable.HOPROFILE_SERVICE_PREFIX + "v1.0/users/" + USER_ID_DYNAMIC_HO + "/validatepin"
+				uri.path = CommonVariable.HOPROFILE_SERVICE_PREFIX + "v1.0/users/" + USER_ID_DYNAMIC_HO + "/phones/"+phoneid+"/verify"
 				println "uri path : "+uri.path
 				headers.'Authorization' = "Bearer "+ ACCESS_TOKEN_DYNAMIC_HO
 				body = json.toString();
@@ -59,14 +65,54 @@ class JobFlowIntegrationTest extends AbstractHomeowner{
 		
 	}
 	
+	
+	
 
 	private def validatePIN(){
 		def getphoneId = DatabaseHelper.select("select phone_id from hoprofile.ho_profile where user_id =  '${USER_ID_DYNAMIC_HO}'")
-		getpinToken = DatabaseHelper.select("select pin_token from hoprofile.phone where id IN ( select phone_id from hoprofile.ho_profile where user_id = '${USER_ID_DYNAMIC_HO}')")
-		if (getpinToken.startsWith("[{pin_token")){
-			getpinToken = getpinToken.replace("[{pin_token=", "").replace("}]","")
+		def getpinTokenTemp = DatabaseHelper.select("select pin_token from hoprofile.phone where id IN ( select phone_id from hoprofile.ho_profile where user_id = '${USER_ID_DYNAMIC_HO}')")
+		if (getpinTokenTemp.startsWith("[{pin_token")){
+			getpinToken = getpinTokenTemp.replace("[{pin_token=", "").replace("}]","")
+			phoneid = getphoneId.replace("[{phone_id=", "").replace("}]","")
 			println "pin token is : " +getpinToken
 		}
+	}
+	
+	private def postPhone(String userId1){
+		
+		String responseStatus = null
+		def json = new JsonBuilder()
+		json {
+			"userId" USER_ID_DYNAMIC_HO
+			"number" CommonVariable.DEFAULT_MOBILE_PREFIX + RANDOM_MOBILE
+		}
+		//HTTPBuilder HTTP_BUILDER = new HTTPBuilder(CommonVariable.SERVER_URL)
+		HTTP_BUILDER.request(Method.POST){
+			uri.path = CommonVariable.HOPROFILE_SERVICE_PREFIX + "v1.0/users/" + USER_ID_DYNAMIC_HO + "/phones"
+			headers.Accept = 'application/json'
+			println "uri path : "+uri.path
+			headers.'Authorization' = "Bearer "+ ACCESS_TOKEN_DYNAMIC_HO
+			body = json.toString();
+			requestContentType = ContentType.JSON
+			println "Uri is " + uri
+			body = json.toString()
+						
+			response.success = { resp, reader ->
+					println "Success"
+					println "Got response: ${resp.statusLine}"
+					responseStatus = resp.statusLine.statusCode
+					println "Content-Type: ${resp.headers.'Content-Type'}"
+					reader.each{ "Results  : "+ "$it" }
+				}
+	
+				response.failure = { resp, reader ->
+					println "Request failed with status ${resp.status}"
+					reader.each{ println "Error values : "+"$it" }
+					responseStatus = resp.statusLine.statusCode
+				}
+		}
+			
+		
 	}
 	
 	private def getToken(){
