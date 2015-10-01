e21adb64d01cc67ddd7925c426da892b8a6b4575
Davide Porracchio <davide.porracchio@ratedpeople.com>
2015-10-01 12:32:23 +0100

    GID-554 updated payment

--- src/test/groovy/com/ratedpeople/payment/resource/CreditCardFunctionalTest.groovy
+++ src/test/groovy/com/ratedpeople/payment/resource/CreditCardFunctionalTest.groovy
@@ -8,6 +8,10 @@ import groovyx.net.http.ContentType
 import groovyx.net.http.Method
 import spock.lang.Specification
 
+import com.ratedpeople.service.HomeownerService
+import com.ratedpeople.service.PaymentService
+import com.ratedpeople.service.utility.ResultInfo
+import com.ratedpeople.service.utility.UserInfo
 import com.ratedpeople.support.CommonVariable
 import com.ratedpeople.support.DatabaseHelper
 /**
@@ -17,115 +21,46 @@ import com.ratedpeople.support.DatabaseHelper
 
 class CreditCardFunctionalTest extends Specification {
 
-	private final String CREDIT_CARD_RESOURCE_URI = CommonVariable.PAYMENT_SERVICE_PREFIX + "v1.0/users/"
-
-	def "setup"(){
-		HTTP_BUILDER.handler.failure = { resp, reader ->
-			[response:resp, reader:reader]
-		}
-		HTTP_BUILDER.handler.success = { resp, reader ->
-			[response:resp, reader:reader]
-		}
-	}
+	private HomeownerService homeownerService = new HomeownerService();
+	private PaymentService paymentService = new PaymentService()
 
 	def testCreditCardSuccess() {
 		println "CreditCardFunctionalTest"
 		println "Test 1 :  testCreditCardSuccess"
-
 		given:
-		String responseStatus
-		String ccToken
+		UserInfo user =  homeownerService.createAndActivateDynamicUser()
+		
 		when:
-		def response = postCreditCard(CREDIT_CARD_RESOURCE_URI,createJsonCreditCard());
-		//CREDIT_CARD_RESOURCE_URI,def uricc,
-		println "RESPONSE : "+response
-		def resp = response['response']
-		def reader = response['reader']
-
-		responseStatus = resp.status
-		println "*********************************"
-		println "response code :${resp.status}"
-		println "reader type: ${reader.get('message')}"
-		println "*********************************"
-
-		reader.each{
-			println "Response data: " + "$it"
-
-			String data = "$it"
-			if (data.startsWith("token")){
-				ccToken = data.replace("token=", "")
-			}
-		}
+		def json = createJsonCreditCard(user);
+		ResultInfo result = paymentService.postCreditCard(user, json)
 		then:
-		responseStatus == CommonVariable.STATUS_201
-		cleanup:
-		DatabaseHelper.executeQuery("delete from payment.credit_card where token = '${ccToken}'")
+		result.getResponseCode().contains(CommonVariable.STATUS_201)
 	}
 
 
 	def testGetCardDetails(){
-		println "Test 2 :  testGetCardDetails"
-
+		println "CreditCardFunctionalTest"
+		println "Test 1 :  testCreditCardSuccess"
+		
 		given:
-		def response = postCreditCard(CREDIT_CARD_RESOURCE_URI,createJsonCreditCard());
-		String ccToken
-		response = getCreditCard(CREDIT_CARD_RESOURCE_URI)
-		def resp = response['response']
-		def reader = response['reader']
-
-		reader.each{
-			println "Response data: " + "$it"
-			String data = "$it"
-			if (data.startsWith("token")){
-				ccToken = data.replace("token=", "")
-				println "ccToken : "+ccToken
-			}
-		}
-
-		assert resp.status.toString() == CommonVariable.STATUS_200
-		println "Credit card fetched"
-
-		String responseStatus
+		UserInfo user =  homeownerService.createAndActivateDynamicUser()
+		def json = createJsonCreditCard(user);
+		paymentService.postCreditCard(user, json)
 		when:
-		def map = HTTP_BUILDER.request(Method.GET) {
-			uri.path = CREDIT_CARD_RESOURCE_URI + USER_ID_DYNAMIC_HO + "/cards"
-			headers.'Authorization' = "Bearer " + ACCESS_TOKEN_DYNAMIC_HO
-			requestContentType = ContentType.JSON
-			headers.Accept = ContentType.JSON
-			println "Get credit card Uri: "+ '${uri}'
-		}
-
-		resp = map['response']
-		reader = map['reader']
-
-		responseStatus = resp.status.toString()
-
-		def getToken
-		reader.each{
-			println "Response data: " + "$it"
-
-			String data = "$it"
-			if (data.startsWith("token")){
-				getToken = data.replace("token=", "")
-			}
-		}
+		ResultInfo result = paymentService.getCreditCard(user)
 		then:
-		responseStatus == CommonVariable.STATUS_200
-		getToken == ccToken
-		cleanup:
-		DatabaseHelper.executeQuery("delete from payment.credit_card where token = '${ccToken}'")
-	}
+		result.getResponseCode().contains(CommonVariable.STATUS_200)
+		
+		}
 
-	def testCreateCreditCardValidation(){
-		println "Test 3 :  testCreateCreditCardValidation"
-		println "User is : "+ USER_ID_DYNAMIC_HO
-
+	/*def testCreateCreditCardValidation(){
 		given:
+		UserInfo user =  homeownerService.createAndActivateDynamicUser()
+		println "Test 3 :  testCreateCreditCardValidation"
+		println "User is : "+ user.getId()
 		String responseStatus = null
 		String errorMessage
-
 		def json = new JsonBuilder()
-
 		json {
 			number creditCardNumber
 			userId USER_ID_DYNAMIC_HO
@@ -138,18 +73,11 @@ class CreditCardFunctionalTest extends Specification {
 
 		println "Json is " +  json.toString()
 		when:
-		def response = postCreditCard(CREDIT_CARD_RESOURCE_URI,json)
-		def resp = response['response']
-		def reader = response['reader']
-
-		println "*********************************"
-		println "response code :${response.status}"
-		println "reader type: ${reader.get('message')}"
-		println "*********************************"
-
+		ResultInfo result = paymentService.postCreditCard(user, json)
+		
 		println "Credit card fetched"
 		String delims = "[-\\,]+"
-		def aspectResultList = message.tokenize(delims)
+		def aspectResultList = result.getResponseCode().tokenize(delims)
 		def resultFound = reader.get('message').trim()
 
 		responseStatus = resp.status.toString()
@@ -167,49 +95,39 @@ class CreditCardFunctionalTest extends Specification {
 		CommonVariable.DEFAULT_CC_NUMBER|CommonVariable.DEFAULT_CC_CVV|CommonVariable.DEFAULT_CC_EXPIRY_YEAR|CommonVariable.DEFAULT_CC_EXPIRY_MONTH|"    "|CommonVariable.DEFAULT_CC_TYPE||CommonVariable.STATUS_400||CommonVariable.CC_NAME_VALIDATION
 		CommonVariable.DEFAULT_CC_NUMBER|CommonVariable.DEFAULT_CC_CVV|CommonVariable.DEFAULT_CC_EXPIRY_YEAR|CommonVariable.DEFAULT_CC_EXPIRY_MONTH|CommonVariable.DEFAULT_CC_NAME| "   "||CommonVariable.STATUS_400||CommonVariable.CARD_TYPE_VALIDATION
 	}
-
+*/
 	def testCreateCreditCardWhenAlreadyExists(){
-		println "Test 3 :  testCreateCreditCardValidation"
-		println "USER IS ${USER_ID_DYNAMIC_HO}"
-
+		
+		
+		
 		given:
-		String ccToken
-		String errorMessage
-		DatabaseHelper.executeQuery("delete from payment.credit_card where token = '${ccToken}'")
-		def response = postCreditCard(CREDIT_CARD_RESOURCE_URI,createJsonCreditCard())
-		def resp = response['response']
-		def reader = response['reader']
-
-		reader.each{
-			println "Response data: " + "$it"
-
-			String data = "$it"
-			if (data.startsWith("token")){
-				ccToken = data.replace("token=", "")
-			}
-		}
-
-		assert resp.status.toString() == CommonVariable.STATUS_201
-		println "Credit card created"
-
-		String responseStatus
+		UserInfo user =  homeownerService.createAndActivateDynamicUser()
+		println "Test 3 :  testCreateCreditCardValidation"
+		println "USER IS ${user.getId()}"
+		def json = createJsonCreditCard(user);
+		paymentService.postCreditCard(user, json)
+		given:
 		when:
-		response = postCreditCard(CREDIT_CARD_RESOURCE_URI,createJsonCreditCard())
-
-		resp = response['response']
-		reader = response['reader']
-
-		responseStatus = resp.status.toString()
-		errorMessage = reader.get('message')
-		println "error Message : "+ errorMessage
-		println "From DataMap : "+ CommonVariable.CARD_ALREADY_EXISTS
+		ResultInfo result = paymentService.postCreditCard(user, json)
 		then:
-		responseStatus == CommonVariable.STATUS_409
-		cleanup:
-		DatabaseHelper.executeQuery("delete from payment.credit_card where token = '${ccToken}'")
+		result.getResponseCode().contains(CommonVariable.STATUS_409)
+		
 	}
 
 
+	public def createJsonCreditCard(UserInfo user){
+		def json = new JsonBuilder()
+		json {
+			"number" (Math.round(Math.random()*100000000000000)+1000000000000000);
+			"userId" user.getId()
+			"cvv" CommonVariable.DEFAULT_CC_CVV
+			"expiryYear" CommonVariable.DEFAULT_CC_EXPIRY_YEAR
+			"expiryMonth" CommonVariable.DEFAULT_CC_EXPIRY_MONTH
+			"nameOnCard" CommonVariable.DEFAULT_CC_NAME
+			"type" CommonVariable.DEFAULT_CC_TYPE
+		}
 
-
+		println "Json is  CC :${json.toString()}"
+		return json;
+	}
 }
